0ab141984adaed07e1ea7d34606e89960e36e333
Bitee-cd <calebduniya45@gmail.com>
2021-11-05 22:40:37 +0100

    first commit for boiler page

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './Styled Components/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Team Tyrell: First Person to Clone should delete this</h1>
+    </div>
+  );
+}
+
+export default App;
