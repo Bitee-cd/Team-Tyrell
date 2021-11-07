a3f322d7aec7c18f54341fe48ad6f4c0b5e00a1e
Theophilus <extheo2019@gmail.com>
2021-11-07 07:12:39 +0100

    added hover state to icons

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,27 +1,25 @@
-import React, {useState} from 'react'
-import '../Styled Components/search.css'
-import SearchIcon from '@material-ui/icons/Search'
-import MicIcon from '@material-ui/icons/Mic'
-import img from '../img/logo.png'
+import React, { useState } from "react";
+import "../Styled Components/search.css";
+import SearchIcon from "@material-ui/icons/Search";
+import MicIcon from "@material-ui/icons/Mic";
+import img from "../img/logo.png";
 
 function Search() {
-    const [input, setInput] = useState("")
-    const h1 = <h1>{input}</h1>
-    const image = <img src={img} alt="logo"/>
+  const [input, setInput] = useState("");
+  const h1 = <h1>{input}</h1>;
+  const image = <img src={img} alt="logo" />;
 
-    return (
-        <div className='search'>
-            <div className='logo'>
-                {input === "" ? image : h1}
-            </div>
+  return (
+    <div className="search">
+      <div className="logo">{input === "" ? image : h1}</div>
 
-            <div className='input_container'>
-                <SearchIcon className='input_icon'/>
-                <input value={input} onChange={e => setInput(e.target.value)}/>
-                <MicIcon />
-            </div>
-        </div>
-    )
+      <div className="input_container">
+        <SearchIcon className="input_icon" />
+        <input value={input} onChange={(e) => setInput(e.target.value)} />
+        <MicIcon className="mic-icon" />
+      </div>
+    </div>
+  );
 }
 
-export default Search
+export default Search;
